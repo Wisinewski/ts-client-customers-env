b1070fd1ee929fe197b5a7168defc81135fc05ba
Wisinewski <g_gomees@hotmail.com>
2021-08-16 14:38:40 -0300

    test: ensure RemoteAddCustomer calls HttpPostClient with correct url

---
+++ src/data/usecases/customer/remote-add-customer.spec.ts
@@ -0,0 +1,27 @@
+import { mockCustomerParams } from '@/domain/test/customer/mock-customer'
+import faker from 'faker'
+import { HttpPostClientSpy } from '@/data/test/mock-http-client'
+import { RemoteAddCustomer } from './remote-add-customer'
+
+type SutTypes = {
+  sut: RemoteAddCustomer
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAddCustomer(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAddCustomer', () => {
+  test('should call HttpPostClient with correct url', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockCustomerParams())
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})

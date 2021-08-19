aa1936762a75c0579ce2d738ffd809a4734f71cf
Wisinewski <g_gomees@hotmail.com>
2021-08-19 14:32:17 -0300

    test: ensure AddCustomer starts with create button disabled

--- src/presentation/pages/add-customer/add-customer.spec.tsx
+++ src/presentation/pages/add-customer/add-customer.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react'
 import AddCustomer from './add-customer'
 
 describe('Name of the group', () => {
-  test('should not render spinner and error on start', () => {
+  test('should start with initial state', () => {
     const { getByTestId } = render(<AddCustomer />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const createButton = getByTestId('createButton') as HTMLButtonElement
+    expect(createButton.disabled).toBe(true)
   })
 })

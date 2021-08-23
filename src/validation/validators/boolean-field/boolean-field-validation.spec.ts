a9bebe3f63719e291f151bab0c2c67cd5424c8f0
Wisinewski <g_gomees@hotmail.com>
2021-08-23 10:22:35 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/boolean-field/boolean-field-validation.spec.ts
+++ src/validation/validators/boolean-field/boolean-field-validation.spec.ts
@@ -2,15 +2,26 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { BooleanFieldValidation } from './boolean-field-validation'
 import faker from 'faker'
 
+type SutTypes = {
+  sut: BooleanFieldValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new BooleanFieldValidation(faker.database.column())
+  return {
+    sut
+  }
+}
+
 describe('BooleanFieldValidation', () => {
   test('should return error if value is not boolean', () => {
-    const sut = new BooleanFieldValidation('ci')
+    const { sut } = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('should return falsy if value is boolean', () => {
-    const sut = new BooleanFieldValidation('ci')
+    const { sut } = makeSut()
     const error = sut.validate(faker.datatype.boolean())
     expect(error).toBeFalsy()
   })

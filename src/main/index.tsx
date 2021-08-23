0ffafec37ec869cf5fc5e06109d4ec265e90ba4b
Wisinewski <g_gomees@hotmail.com>
2021-08-23 13:47:44 -0300

    feat: add AddCustomer composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from '@/presentation/components/router/router'
+import { makeAddCustomer } from './factories/pages/add-customer/add-customer-factory'
 
 ReactDOM.render(
-  <Router />,
+  <Router
+    makeAddCustomer={makeAddCustomer}
+  />,
   document.getElementById('main')
 )
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,15 +1,18 @@
-import AddCustomer from '@/presentation/pages/add-customer/add-customer'
 import Dashboard from '@/presentation/pages/dashboard/dashboard'
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '../../styles/global.scss'
 
-const Router: React.FC = () => {
+type Props = {
+  makeAddCustomer: React.FC
+}
+
+const Router: React.FC<Props> = ({ makeAddCustomer }: Props) => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Dashboard} />
-        <Route path="/add" exact component={AddCustomer} />
+        <Route path="/add" exact component={makeAddCustomer} />
       </Switch>
     </BrowserRouter>
   )

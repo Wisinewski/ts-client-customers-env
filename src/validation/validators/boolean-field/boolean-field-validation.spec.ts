0f6d0295e465d3baa6d51eae4cb9f61e2b48a19f
Wisinewski <g_gomees@hotmail.com>
2021-08-23 10:20:02 -0300

    test: ensure BooleanFieldValidation returns falsy if field value is boolean

--- src/validation/validators/boolean-field/boolean-field-validation.spec.ts
+++ src/validation/validators/boolean-field/boolean-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { BooleanFieldValidation } from './boolean-field-validation'
+import faker from 'faker'
 
 describe('BooleanFieldValidation', () => {
   test('should return error if value is not boolean', () => {
     const sut = new BooleanFieldValidation('ci')
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return falsy if value is boolean', () => {
+    const sut = new BooleanFieldValidation('ci')
+    const error = sut.validate(faker.datatype.boolean())
+    expect(error).toBeFalsy()
   })
 })

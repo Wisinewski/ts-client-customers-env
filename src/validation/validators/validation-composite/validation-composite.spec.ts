c5cd597dc92f1bdf0a0339a8546dc6c7c5552261
Wisinewski <g_gomees@hotmail.com>
2021-08-23 10:49:22 -0300

    test: ensure ValidationComposite returns the first error received

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,15 +5,16 @@ import faker from 'faker'
 describe('ValidationComposite', () => {
   test('should return error if any validation fails', () => {
     const field = faker.database.column()
-    const errorText = faker.random.words()
+    const firstError = faker.random.words()
     const fieldValidationSpy = new FieldValidationSpy(field)
+    fieldValidationSpy.error = new Error(firstError)
     const fieldValidationSpy2 = new FieldValidationSpy(field)
-    fieldValidationSpy2.error = new Error(errorText)
+    fieldValidationSpy2.error = new Error('second_error')
     const sut = new ValidationComposite([
       fieldValidationSpy,
       fieldValidationSpy2
     ])
     const error = sut.validate(field, faker.random.word())
-    expect(error).toBe(errorText)
+    expect(error).toBe(firstError)
   })
 })
